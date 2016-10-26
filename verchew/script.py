04d436ae1e2a65e4d057d97f11eb53cec3b10be4
Jace Browning <jacebrowning@gmail.com>
2016-10-26 14:13:46 -0400

    Bump version to 0.5b1

--- verchew/script.py
+++ verchew/script.py
@@ -36,7 +36,7 @@ from collections import OrderedDict
 from subprocess import Popen, PIPE, STDOUT
 import logging
 
-__version__ = '0.4'
+__version__ = '0.5b1'
 
 PY2 = sys.version_info[0] == 2
 CONFIG_FILENAMES = ['.verchew', '.verchewrc', 'verchew.ini', '.verchew.ini']
